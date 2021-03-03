a6934dccfee6be7d7458050055327c3e249e057b
twolak88 <twolak88@gmail.com>
2021-03-03 23:12:39 +0100

    Integrating Angular and Spring Boot adding CrossOrigin to let call services by JavaScript

--- src/main/java/com/twolak/springframework/springecommercebackend/repositories/ProductRepository.java
+++ src/main/java/com/twolak/springframework/springecommercebackend/repositories/ProductRepository.java
@@ -4,7 +4,9 @@ import com.twolak.springframework.springecommercebackend.domain.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }

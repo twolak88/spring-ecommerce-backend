2ca4265928d14c146df7c49a5ce88805cd3fd342
twolak88 <twolak88@gmail.com>
2021-03-03 19:04:10 +0100

    Initial project created by spring.io

---
+++ src/main/java/com/twolak/springframework/springecommercebackend/SpringEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.twolak.springframework.springecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twolak/springframework/springecommercebackend/SpringEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twolak.springframework.springecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

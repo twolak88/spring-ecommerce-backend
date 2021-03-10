7cc739b740dc61905334771a23e2ef5c1f815a8b
twolak88 <twolak88@gmail.com>
2021-03-10 19:37:13 +0100

    Adding Order/Purchase Entities

---
+++ src/main/java/com/twolak/springframework/springecommercebackend/domain/Order.java
@@ -0,0 +1,81 @@
+package com.twolak.springframework.springecommercebackend.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "order_tracking_number")
+  private String orderTrackingNumber;
+
+  @Column(name = "total_quantity")
+  private Integer totalQuantity;
+
+  @Column(name = "total_price")
+  private BigDecimal totalPrice;
+
+  @Column(name = "status")
+  private String status;
+
+  @Column(name = "date_created")
+  @CreationTimestamp
+  private Date dateCreated;
+
+  @Column(name = "last_updated")
+  @UpdateTimestamp
+  private Date lastUpdated;
+
+  @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+  private Set<OrderItem> orderItems = new HashSet<>();
+
+  @ManyToOne
+  @JoinColumn(name = "customer_id")
+  private Customer customer;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "shipping_address_id", referencedColumnName = "id")
+  private Address shippingAddress;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "billing_address_id", referencedColumnName = "id")
+  private Address billingAddress;
+
+  public void add(OrderItem orderItem) {
+    if (orderItem != null) {
+      if (this.orderItems == null) {
+        this.orderItems = new HashSet<>();
+      }
+      this.orderItems.add(orderItem);
+      orderItem.setOrder(this);
+    }
+  }
+}

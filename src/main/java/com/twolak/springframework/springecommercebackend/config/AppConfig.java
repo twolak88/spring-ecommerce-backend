a31ece40e4853379f66639660e707f80e94acfbe
twolak88 <twolak88@gmail.com>
2021-03-12 22:22:00 +0100

    move cors configuration for RestControlers into configuration file

---
+++ src/main/java/com/twolak/springframework/springecommercebackend/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.twolak.springframework.springecommercebackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+  
+  @Value("${spring.data.rest.base-path}")
+  private String basePath;
+
+  @Value("${allowed.origins}")
+  private String[] allowedOrigins;
+
+  @Override
+  public void addCorsMappings(CorsRegistry cors) {
+    cors.addMapping(this.basePath + "/**").allowedOrigins(this.allowedOrigins);
+  }
+}
--- src/main/java/com/twolak/springframework/springecommercebackend/controller/CheckoutController.java
+++ src/main/java/com/twolak/springframework/springecommercebackend/controller/CheckoutController.java
@@ -4,13 +4,11 @@ import com.twolak.springframework.springecommercebackend.dto.Purchase;
 import com.twolak.springframework.springecommercebackend.dto.PurchaseResponse;
 import com.twolak.springframework.springecommercebackend.service.CheckoutService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
